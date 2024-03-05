9c337197fc74cd32e528b4c3fbd812cc4898de80
tisiphonee <122160526+tisiphonee@users.noreply.github.com>
2024-03-05 21:56:14 +0330

    Fix Loading A and B into register (Considered real register with load option)

--- 1.v
+++ 1.v
@@ -2,18 +2,23 @@ module fixed_point_division
   (
    input clk, 
    input rst,  
+   input start,
+   input ld_a,
+   input ld_b,
    input [9:0] A,
    input [9:0] B, 
    output reg [9:0] q,
    output reg ov
    );
 
+   reg [9:0] local_A;
+   reg [9:0] local_B;
    reg [10:0] ACC;
    reg [9:0] Q;
    reg [10:0] ACC_next;
    reg [9:0] Q_next;
 
-   always @ (posedge clk or posedge rst)
+   always @(posedge clk or posedge rst)
    begin
       if (rst)
       begin
@@ -21,17 +26,17 @@ module fixed_point_division
          Q <= 10'b0;
          ov <= 0;
       end
-      else
+      else if (start)
       begin
          {ACC_next, Q_next} = {ACC[9:0], Q, 1'b0};
 
-         if (ACC >= {1'b0, B})
+         if (ACC >= {1'b0, local_B})
          begin
-            ACC_next = ACC - {1'b0, B};
+            ACC_next = ACC - {1'b0, local_B};
             {ACC_next, Q_next} = {ACC_next[9:0], Q, 1'b1};
          end
 
-         if (Q_next[9:4] != 0 /*&& (i == 9)*/)
+         if (Q_next[9:4] != 0)
          begin
             ov = 1;
          end
@@ -40,6 +45,30 @@ module fixed_point_division
             ACC = ACC_next;
             Q = Q_next;
          end
+      end
+   end
+
+   always @(posedge clk or posedge rst)
+   begin
+      if (rst)
+      begin
+         local_A <= 10'b0;
+      end
+      else if (ld_a)
+      begin
+         local_A <= A;
+      end
+   end
+
+   always @(posedge clk or posedge rst)
+   begin
+      if (rst)
+      begin
+         local_B <= 10'b0;
+      end
+      else if (ld_b)
+      begin
+         local_B <= B;
       end
    end
 
--- 1_tb.v
+++ 1_tb.v
@@ -2,13 +2,16 @@ module testbench;
 
    reg clk;
    reg rst;
+   reg start;
+   reg ld_a;
+   reg ld_b;
    reg [9:0] A;
    reg [9:0] B;
    wire [9:0] q;
    wire ov;
 
-   fixed_point_division  uut(clk,rst,A,B,q,ov);
-   clk = 0;
+   fixed_point_division  uut(clk, rst, start, ld_a, ld_b, A, B, q, ov);
+   assign clk = 0;
    initial
    begin
       forever #5 clk = ~clk;
@@ -19,9 +22,13 @@ module testbench;
       A = 10'b0000100000;  
       B = 10'b0000010000; 
       rst = 1;
+      start = 0;
+      ld_a = 1;
+      ld_b = 1;
 
       #10 rst = 0;
-      #20;
+      start = 0;
+      #30;
 
       $display("time=%0t, A=%b, B=%b, q=%b, ov=%b", $time, A, B, q, ov);
       #100;
