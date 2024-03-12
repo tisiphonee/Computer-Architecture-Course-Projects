77f19e4f4907dd610f4ce3460b375b4485053f45
tisiphonee <122160526+tisiphonee@users.noreply.github.com>
2024-03-12 13:18:34 +0330

    .

--- gen.py
+++ gen.py
@@ -2,23 +2,38 @@ import random
 
 
 def gen_rnd():
-    return format(random.randint(0, 2**10 - 1), '010b')
+    return format(random.randint(2**4, 2**10 - 1), '010b')
+
+
+def gen_rnd_2():
+    return format(random.randint(1, 2**2 - 1), '010b')
 
 
 def div(A, B):
     int_A = int(A, 2)
     int_B = int(B, 2)
+
     res = int_A / int_B
     bin = format(int(res * (2 ** 4)), '010b')
     return bin
 
 
 data = []
+i = 0
 for _ in range(100):
     A = gen_rnd()
-    B = gen_rnd()
+    B = gen_rnd_2()
     Q = div(A, B)
+    float_A = int(A, 2) / (2 ** 4)
+    float_B = int(B, 2) / (2 ** 4)
+    Q_res = int(Q, 2) / (2 ** 4)
+    # print(float_A, float_B, float_A/float_B, Q_res, Q)
+    if Q_res != (float_A/float_B):
+        Q = "OVERFLOW!"
+        i += 1
     data.append((A, B, Q))
+
+print(i)
 
 with open('input.txt', 'w') as file:
     for A, B, Q in data:
