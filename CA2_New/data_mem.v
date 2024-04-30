24aba80b4eb4dafb2b947def146c469a270e209b
tisiphonee <122160526+tisiphonee@users.noreply.github.com>
2024-04-30 10:33:53 +0330

    Done Gen

--- CA2_New/data_mem.v
+++ CA2_New/data_mem.v
@@ -5,7 +5,7 @@ module DataMemory (input [31:0] A, WD, input WE , clk , output reg[31:0] RD);
     reg [31:0] adr;
     assign adr = {A[31:2], 2'b00}; 
 
-    initial $readmemb("data.mem", dataMem);
+    initial $readmemb("test_cases/arr1.mem", dataMem);
     //initial $readmemb("data.mem", dataMem, 1000);
 
     always @(posedge clk) begin
